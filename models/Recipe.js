3e2a503abf96356caacfeb02084a39d5930b604a
mSinisa <msinisa602@yahoo.com>
2020-04-18 19:58:18 +0200

    updated models

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const recipeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    duration: {
-        type: String
-    },
-    image: {
-        type: String
-    },
-    owner: {
-        id: { 
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
-    },
-    steps: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Step'
-    }]
-});
+	name: {
+		type: String,
+		required: [true, 'Name is required'],
+	},
+	description: {
+		type: [true, 'Description is required'],
+	},
+	duration: {
+		type: String,
+	},
+	image: {
+		type: String,
+	},
+	owner: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		username: String,
+	},
+	steps: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Step',
+		},
+	],
+})
 
-module.exports = mongoose.model('Recipe', recipeSchema);
+module.exports = mongoose.model('Recipe', recipeSchema)
