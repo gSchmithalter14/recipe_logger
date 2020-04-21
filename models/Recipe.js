2b1765c68a70c6051c54a0cbd8fa6efa879f1ae7
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-04-21 18:18:27 -0500

    fixed description in recipeModel.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,33 +1,34 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-	name: {
-		type: String,
-		required: [true, 'Name is required'],
-	},
-	description: {
-		type: [true, 'Description is required'],
-	},
-	duration: {
-		type: String,
-	},
-	image: {
-		type: String,
-	},
-	owner: {
-		id: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-		username: String,
-	},
-	steps: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Step',
-		},
-	],
-})
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+  },
+  duration: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  owner: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    username: String,
+  },
+  steps: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Step',
+    },
+  ],
+});
 
-module.exports = mongoose.model('Recipe', recipeSchema)
+module.exports = mongoose.model('Recipe', recipeSchema);
