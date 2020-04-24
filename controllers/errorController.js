a48390a80a015007984d02d5da3b19cec34698f8
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-04-24 02:13:34 -0500

    route protection and authorization middleware

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,6 +17,12 @@ const handleValidationErrorDB = (err) => {
   return new ErrorResponse(message, 400);
 };
 
+const handleJWTError = () =>
+  new ErrorResponse('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+  new ErrorResponse('Your token has expired. Please log in again!', 401);
+
 const sendError = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
@@ -51,6 +57,8 @@ module.exports = (err, req, res, next) => {
   if (error.name === 'CastError') error = handleCastErrorDB(error);
   if (error.code === 11000) error = handleDuplicateDB();
   if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+  if (error.name === 'JsonWebTokenError') error = handleJWTError();
+  if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
   sendError(error, res);
 };
