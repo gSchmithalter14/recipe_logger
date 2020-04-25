3f2f3670498dd75df49b60b1faf352f04ba3caf2
mSinisa <msinisa602@yahoo.com>
2020-04-25 16:28:26 +0200

    added step controller and step routes

---
+++ controllers/stepController.js
@@ -0,0 +1,90 @@
+const Recipe = require('../models/Recipe');
+const Step = require('../models/Step');
+const catchAsync = require('../utils/catchAsync');
+const ErrorResponse = require('../utils/errorResponse');
+
+//@desc    Get steps from recipe
+//@route   GET /api/v1/recipes/:id/steps
+//@access  Private
+exports.getSteps = catchAsync(async (req, res, next) => {
+  const recipe = await Recipe.findById(req.params.id).populate('steps');
+  const steps = recipe.steps;
+
+  res.status(200).json({
+    status: 'success',
+    results: steps.length,
+    data: {
+      steps
+    }
+  });
+});
+
+//@desc    Create new recipe step
+//@route   POST /api/v1/recipes/:id/steps
+//@access  Private
+exports.createStep = catchAsync(async (req, res, next) => {
+  const newStep = await Step.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Step successfully created',
+    data: {
+      step: newStep
+    }
+  });
+});
+
+//@desc    Get a step by id
+//@route   GET /api/v1/recipes/:id/steps/:stepId
+//@access  Public
+exports.getStep = catchAsync(async (req, res, next) => {
+  const step = await Step.findById(req.params.stepId);
+
+  if (!step) {
+    return next(new ErrorResponse('No step found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      step
+    }
+  });
+});
+
+//@desc    Update a recipe step
+//@route   PATCH /api/v1/recipes/:id/steps/:stepId
+//@access  Private
+exports.updateStep = catchAsync(async (req, res, next) => {
+  const step = await Step.findByIdAndUpdate(req.params.stepId, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!step) {
+    return next(new ErrorResponse('No step found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Step successfully updated',
+    data: {
+      step
+    }
+  });
+});
+
+//@desc    Delete a recipe step
+//@route   DELETE /api/v1/recipes/:id/steps/:stepId
+//@access  Private
+exports.deleteStep = catchAsync(async (req, res, next) => {
+  const step = await Step.findByIdAndDelete(req.params.stepId);
+  if (!step) {
+    return next(new ErrorResponse('No step found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Step deleted'
+  });
+});
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const recipeController = require('../controllers/recipeController');
 const authController = require('../controllers/authController');
+const stepController = require('../controllers/stepController');
+
+const { protect, restrictTo } = authController;
 
 const {
   getRecipes,
@@ -11,7 +14,13 @@ const {
   deleteRecipe
 } = recipeController;
 
-const { protect, restrictTo } = authController;
+const {
+  getSteps,
+  createStep,
+  getStep,
+  deleteStep,
+  updateStep
+} = stepController;
 
 const router = express.Router();
 
@@ -25,5 +34,16 @@ router
   .get(protect, restrictTo('user', 'admin'), getRecipe)
   .patch(protect, restrictTo('user', 'admin'), updateRecipe)
   .delete(protect, restrictTo('user', 'admin'), deleteRecipe);
+
+router
+  .route('/:id/steps')
+  .get(protect, restrictTo('user', 'admin'), getSteps)
+  .post(protect, restrictTo('user', 'admin'), createStep);
+
+router
+  .route('/:id/steps/:stepId')
+  .get(protect, restrictTo('user', 'admin'), getStep)
+  .patch(protect, restrictTo('user', 'admin'), updateStep)
+  .delete(protect, restrictTo('user', 'admin'), deleteStep);
 
 module.exports = router;
