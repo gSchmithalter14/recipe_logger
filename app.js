b5d78d69db43836cb0082c986a48e51bb3bb849a
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-06-16 19:44:55 -0500

    removed trailing commas

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const mongoSanitize = require("express-mongo-sanitize");
-const cors = require("cors");
-const xss = require("xss-clean");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const hpp = require("hpp");
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
-const ErrorResponse = require("./utils/errorResponse");
-const globalErrorHandler = require("./controllers/errorController");
-const recipeRouter = require("./routes/recipeRoutes");
-const userRouter = require("./routes/userRoutes");
-const authRouter = require("./routes/authRoutes");
+const ErrorResponse = require('./utils/errorResponse');
+const globalErrorHandler = require('./controllers/errorController');
+const recipeRouter = require('./routes/recipeRoutes');
+const userRouter = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
@@ -34,7 +34,7 @@ app.use(xss());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 100, //10 minutes
-  max: 100, //100 requests
+  max: 100 //100 requests
 });
 
 app.use(limiter);
@@ -44,14 +44,14 @@ app.use(hpp());
 
 app.use(cors({ credentials: true, origin: true }));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // ROUTES
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/recipes", recipeRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/recipes', recipeRouter);
+app.use('/api/v1/users', userRouter);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new ErrorResponse(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
